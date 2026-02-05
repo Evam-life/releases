6156d05f469623f4ff1ec7ff4b621a570c6a0eeb
Evam <admin@evam.life>
2026-02-05 13:00:04 +0000

    Automated bump to website version 0.0.2

--- _static/script.js
+++ _static/script.js
@@ -1,6 +1,9 @@
 console.log("Hi")
 
 function fixIndexLink() {
+    if (window.location.pathname === "/"){
+        window.location.replace("/releases/vehicle-services-stable.html")
+    }
     document.getElementsByClassName('md-header-nav__button md-logo')[0].href = "https://www.support.evam.life/en/change_log_VS.html";
 }
 
